657409636323acd819b83d277b11799452884742
khushihk2007-dot <khushi.h.k.2007@gmail.com>
2026-09-07 22:05:31 +0530

    Update map tiles to use CARTO Dark Matter
    Replaced the dark-themed map tiles with CARTO Dark Matter tiles.

--- frontend/src/components/MapView.tsx
+++ frontend/src/components/MapView.tsx
@@ -51,11 +51,12 @@ export default function MapView({
         ref={mapRef}
         attributionControl={true}
       >
-        {/* Dark-themed map tiles (Stadia Alidade Smooth Dark – free) */}
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://stadiamaps.com/">Stadia</a>'
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-        />
+        {/* Dark-themed map tiles (CARTO Dark Matter – free, no API key needed) */}
+<TileLayer
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+  subdomains="abcd"
+/>  
 
         <FlyToStation station={selectedStation} />
 
